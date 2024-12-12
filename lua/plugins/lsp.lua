2732138060699c4be795577ece99083a42bd7ab8
Florian Schuck <hey@flo.fish>
2024-12-12 15:54:42 +0100

    install fidget

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,32 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-	dependencies = {
-		  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
       },
     },
+    config = function()
+      require("lspconfig").lua_ls.setup({})
+    end,
   },
-	},
-        config = function()
-            require("lspconfig").lua_ls.setup {}
-        end,
+  {
+    "williamboman/mason.nvim",
+    config = true, -- Automatically sets up Mason
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls" },
     },
-    {
-        "williamboman/mason.nvim",
-        config = true, -- Automatically sets up Mason
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = { "lua_ls" },
-        },
-    },
+  },
+  { "j-hui/fidget.nvim", opts = {} },
 }
-
