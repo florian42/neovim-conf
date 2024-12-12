bd542bd589d586d3e2d91be683b8d753b6446585
Florian Schuck <hey@flo.fish>
2024-12-12 16:06:15 +0100

    typescript tools and more lsp shortcuts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,67 @@ return {
     },
     config = function()
       require("lspconfig").lua_ls.setup({})
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+        callback = function(event)
+          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
+          -- to define small helper and utility functions so you don't have to repeat yourself.
+          --
+          -- In this case, we create a function that lets us more easily define mappings specific
+          -- for LSP related items. It sets the mode, buffer and description for us each time.
+          local map = function(keys, func, desc, mode)
+            mode = mode or "n"
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          -- Jump to the definition of the word under your cursor.
+          --  This is where a variable was first declared, or where a function is defined, etc.
+          --  To jump back, press <C-t>.
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+          -- Find references for the word under your cursor.
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+          -- Jump to the type of the word under your cursor.
+          --  Useful when you're not sure what type a variable is and you want to see
+          --  the definition of its *type*, not where it was *defined*.
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+          -- Fuzzy find all the symbols in your current document.
+          --  Symbols are things like variables, functions, types, etc.
+          local showSymbolFinder = function()
+            -- https://github.com/nvim-telescope/telescope.nvim/issues/901
+            -- lowercase for simplicity :)
+            local lsp_symbols = vim.tbl_map(string.lower, vim.lsp.protocol.SymbolKind)
+            -- define a filter function to excl. undesired symbols
+            local symbols = vim.tbl_filter(function(symbol)
+              return symbol ~= "property" and symbol ~= "variable"
+            end, lsp_symbols)
+            require("telescope.builtin").lsp_document_symbols({ symbols = symbols })
+          end
+          map("<leader>ds", showSymbolFinder, "[D]ocument [S]ymbols")
+
+          -- Fuzzy find all the symbols in your current workspace.
+          --  Similar to document symbols, except searches over your entire project.
+          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+          -- Rename the variable under your cursor.
+          --  Most Language Servers support renaming across files, etc.
+          map("<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename")
+
+          -- Execute a code action, usually your cursor needs to be on top of an error
+          -- or a suggestion from your LSP for this to activate.
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+          -- WARN: This is not Goto Definition, this is Goto Declaration.
+          --  For example, in C this would take you to the header.
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        end,
+      })
     end,
   },
   {
@@ -29,4 +90,21 @@ return {
     },
   },
   { "j-hui/fidget.nvim", opts = {} },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      expose_as_code_action = { "remove_unused_imports", "add_missing_imports" },
+      tsserver_file_preferences = { importModuleSpecifierPreference = "non-relative" },
+    },
+    config = function(_, opts)
+      vim.keymap.set(
+        "n",
+        "<leader>co",
+        ":TSToolsRemoveUnusedImports<CR>",
+        { noremap = true, silent = true, desc = "Remove unused imports" }
+      )
+      require("typescript-tools").setup(opts)
+    end,
+  },
 }
