b739b5a041484655074433920dce5acbbd0494c6
Florian Schuck <hey@flo.fish>
2024-12-13 10:03:42 +0100

    more options configured

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,6 @@
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.swapfile = false
+vim.opt.splitkeep = "screen"
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -8,21 +8,25 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
+
+vim.opt.autowrite = true -- Enable auto write
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Save undo history
 vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
@@ -36,10 +40,10 @@ vim.opt.timeoutlen = 300
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -48,3 +52,21 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 vim.opt.smoothscroll = true
+
+vim.opt.expandtab = true -- Use spaces instead of tabs
+
+vim.opt.grepprg = "rg --vimgrep"
+
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.smartindent = true -- Insert indents automatically
+
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+
+vim.opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
+
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+
+vim.opt.termguicolors = true -- True color support
+
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
