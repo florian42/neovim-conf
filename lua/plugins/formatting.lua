0a25f7db4ce4e8f1afb1950c7abead6461cf4784
Florian Aumeier <hey@flo.fish>
2025-01-06 16:33:55 +0100

    configure formatting and linting
    Signed-off-by: Florian Aumeier <hey@flo.fish>

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -19,6 +19,9 @@ return {
         -- You can customize some of the format options for the filetype (:help conform.format)
         -- Conform will run the first available formatter
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
       },
     })
   end,
