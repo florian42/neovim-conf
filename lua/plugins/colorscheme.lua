51a5bcf73460828e09523d0de9fed1d67be53565
Florian Aumeier <hey@flo.fish>
2025-06-12 16:25:29 +0200

    update colorschemes, configure oil to my needs
    Signed-off-by: Florian Aumeier <hey@flo.fish>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,38 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme("tokyonight")
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    opts = {},
+    init = function()
+      -- vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    -- lazy = false,
+    -- priority = 1000,
+    name = "rose-pine",
+    config = function()
+      -- vim.cmd("colorscheme rose-pine-dawn")
+    end,
+  },
+  {
+    "cormacrelf/dark-notify",
+    config = function()
+      require("dark_notify").run()
+    end,
+  },
 }
--- lua/plugins/filesystem.lua
+++ lua/plugins/filesystem.lua
@@ -1,16 +1,31 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   version = "*",
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
-    delete_to_trash = false,
+    delete_to_trash = true,
+    lsp_file_methods = {
+      -- Enable or disable LSP file operations
+      enabled = true,
+      -- Time to wait for LSP file operations to complete before skipping
+      timeout_ms = 10000,
+      -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+      -- Set to "unmodified" to only save unmodified buffers
+      autosave_changes = false,
+    },
   },
-  config = function (_, opts)
-	  require("oil").setup(opts)
+  config = function(_, opts)
+    require("oil").setup(opts)
   end,
   keys = {
-        { "<leader>of", function() require("oil").open() end, desc = "Open Oil" },
+    {
+      "<leader>of",
+      function()
+        require("oil").open()
+      end,
+      desc = "Open Oil",
+    },
   },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
