be83c988e76a452831ddeea7a9856a2dee61435c
Florian Aumeier <hey@flo.fish>
2025-10-24 10:26:43 +0200

    update

--- lua/plugins/filesystem.lua
+++ lua/plugins/filesystem.lua
@@ -10,7 +10,7 @@ return {
         -- Enable or disable LSP file operations
         enabled = true,
         -- Time to wait for LSP file operations to complete before skipping
-        timeout_ms = 10000,
+        timeout_ms = 60000,
         -- Set to true to autosave buffers that are updated with LSP willRenameFiles
         -- Set to "unmodified" to only save unmodified buffers
         autosave_changes = false,
