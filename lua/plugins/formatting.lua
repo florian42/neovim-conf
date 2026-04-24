5ee17bf5a0f4922cc677c485a5fee0d58369e5d1
Florian Aumeier <hey@flo.fish>
2026-04-24 10:14:30 +0200

    updates

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,7 +1,7 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    python = { "black" },
+    python = { "ruff_format" },
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
     javascriptreact = { "prettierd", "prettier", stop_after_first = true },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ lint.linters_by_ft = {
   typescript = { "eslint" },
   javascript = { "eslint" },
   yaml = { "cfn_lint" },
-  python = { "flake8" },
+  python = { "ruff" },
 }
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
